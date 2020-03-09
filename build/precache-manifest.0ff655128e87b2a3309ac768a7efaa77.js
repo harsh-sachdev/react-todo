f94e31b3e90291472637c1f9e05c4c96acda7b7b
Harsh <harsh.sachdev@inverv.com>
2020-03-10 00:59:11 +0530

    prod build

---
+++ build/precache-manifest.0ff655128e87b2a3309ac768a7efaa77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8e5856b81d5495a6dfb9a06afc68589",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bc9102721b04d6d233c",
+    "url": "./static/css/main.c28df411.chunk.css"
+  },
+  {
+    "revision": "36af1d5673c0f3b7b6a7",
+    "url": "./static/js/2.020889f7.chunk.js"
+  },
+  {
+    "revision": "35e9035c4541049d41232fe0bf96f70b",
+    "url": "./static/js/2.020889f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bc9102721b04d6d233c",
+    "url": "./static/js/main.162f38b0.chunk.js"
+  },
+  {
+    "revision": "9be72544028eed1cd4a6",
+    "url": "./static/js/runtime-main.044e0bb5.js"
+  }
+]);
